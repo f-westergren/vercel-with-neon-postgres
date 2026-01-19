d47aeb99f527c04792981e8f4fddd60be9105c1e
Folke Westergren <48532419+f-westergren@users.noreply.github.com>
2026-01-19 18:53:18 +0100

    lets try this

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+    
+    if (evt.type === 'user.created') {
+      const { id: clerkId, email_addresses } = evt.data;
+      const email = email_addresses[0]?.email_address;
+
+      if (!email) {
+        return new Response('No Email', { status: 400 });
+      }
+
+      await prisma.user.create({
+        data: { clerkId, email }
+      })
+    }
+    return new Response('OK', { status: 200 });
+  } catch (err) {
+    console.error('Webhook error', err);
+    return new Response('Webhook failed', { status: 400 })
+  }
+}
---
+++ app/sign-up/[[...rest]]/page.tsx
@@ -0,0 +1,5 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <div className="grid w-screen place-item-center"><SignUp /></div>
+}
