3d1fb695d0375c59a532e7192b5e08939a320c3c
Folke Westergren <48532419+f-westergren@users.noreply.github.com>
2026-01-27 15:22:20 +0100

    lets see what happens

--- app/sign-up/[[...rest]]/page.tsx
+++ app/sign-up/[[...rest]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <div className="grid w-screen place-item-center"><SignUp /></div>
+  return <div className="grid w-screen place-item-center"><SignUp forceRedirectUrl='/dashboard' /></div>
 }
