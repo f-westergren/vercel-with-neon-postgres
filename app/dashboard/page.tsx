225936001715e3bbc24326cda5c1e531a5fc7cd0
Folke Westergren <48532419+f-westergren@users.noreply.github.com>
2026-01-27 15:06:44 +0100

    couple of changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="mx-auto flex w-full max-w-md flex-1 flex-col px-5 md:max-w-lg md:px-0 lg:max-w-xl">
-        <p>hallo!</p>
+        <p>This is the dashboard ya'll!</p>
       </div>
     </div>
   );
