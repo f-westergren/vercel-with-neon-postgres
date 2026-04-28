86bce5c82a31a2e8e05e053439e2a18daf06c586
Folke Westergren <48532419+f-westergren@users.noreply.github.com>
2026-04-28 09:38:20 +0200

    change prop

--- app/sign-up/[[...rest]]/page.tsx
+++ app/sign-up/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="grid min-h-screen w-screen place-items-center">
-      <SignUp forceRedirectUrl="/dashboard" />
+      <SignUp fallbackRedirectUrl="/dashboard" />
     </div>
   );
 }
