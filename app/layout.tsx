f508a02edc98840acb49938bfeccd0346f690f2f
Folke Westergren <48532419+f-westergren@users.noreply.github.com>
2026-06-24 09:24:44 +0200

    remove proxy

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider proxyUrl="https://noeli.dev/__clerk">
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
